7e76ec4dd20367fb5dd9be576232d6f0ca6bacf2
Juan D. Jara <juanorigami@gmail.com>
2020-01-13 00:11:09 +0100

    refactor user socket backend

--- api/socketHandlers/users.socket.js
+++ api/socketHandlers/users.socket.js
@@ -3,15 +3,18 @@ module.exports = function (socket, io) {
   socket.on('disconnect', () => {
     socket.broadcast.emit('user:left', socket.id)
   })
+  socket.on('user:leave', () => {
+    socket.broadcast.emit('user:left', socket.id)
+  })
   socket.on('user:join', ({ name, room }) => {
     const roomId = `public-${room}`
     socket.join(roomId, () => {
-      io.to(roomId).emit('user:joined', { id: socket.id, name, roomId })
+      socket.broadcast.emit('user:joined', { id: socket.id, name, room })
     })
   })
   socket.on('user:id-request', (callback) => callback({ id: socket.id, name }))
-  socket.on('user:list-request', room => {
-    socket.to(room).emit('user:list-request', socket.id)
+  socket.on('user:list-request', () => {
+    socket.broadcast.emit('user:list-request', socket.id)
   })
   socket.on('user:list-response', ({ receiver, sender }) => {
     socket.to(receiver).emit('user:list-response', ({ ...sender, id: socket.id }))
