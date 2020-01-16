9d6dcc9064e029d3b130cfbd1f720f917665ab3b
Juan D. Jara <juanorigami@gmail.com>
2020-01-17 00:16:11 +0100

    deck list wireframe

--- www/src/pages/DeckList.js
+++ www/src/pages/DeckList.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
+import Button from '../components/Button'
+
+const DeckListStyles = styled.div`
+  margin: 0 auto;
+  max-width: 1200px;
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    h2 {
+      margin: 12px 0;
+    }
+  }
+`
 
 export default function DeckList () {
   return (
-    <div> Deck List </div>
+    <DeckListStyles>
+      <header>
+        <h2>Mis cartas</h2>
+        <Button>Crear mazo</Button>
+      </header>
+      <p>No tienes ningún mazo aún</p>
+    </DeckListStyles>
   )
 }
