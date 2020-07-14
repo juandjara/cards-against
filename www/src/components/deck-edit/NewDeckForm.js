3f08a78866ae0b81fee0ae7f0bcc4de6f1943f6a
Juan D. Jara <juanorigami@gmail.com>
2020-07-15 00:09:50 +0200

    extract new deck form styles

--- www/src/components/deck-edit/NewDeckForm.js
+++ www/src/components/deck-edit/NewDeckForm.js
@@ -1,6 +1,40 @@
 import React, { useState } from 'react'
 import InputStyles from '../Input'
 import Button from '../Button'
+import styled from 'styled-components'
+
+const DeckFormStyles = styled.form`
+  label {
+    font-size: 12px;
+    color: var(--colorMidHigh);
+    display: block;
+    margin-bottom: 4px;
+  }
+
+  input {
+    margin-bottom: 1.5rem;
+  }
+
+  textarea {
+    margin-bottom: 1.5rem;
+    font: inherit;
+    padding-top: 4px;
+    height: 80px;
+    resize: none;
+  }
+
+  input, textarea {
+    max-width: 320px;
+  }
+
+  .cancel-btn {
+    background: none;
+    border: none;
+    &:hover, &:focus {
+      text-decoration: underline;
+    }
+  }
+`
 
 export default function NewDeckForm ({ deck = {}, onSubmit }) {
   const [name, setName] = useState(deck.name || '')
@@ -11,8 +45,9 @@ export default function NewDeckForm ({ deck = {}, onSubmit }) {
   }
 
   return (
-    <form className="new-deck-form" onSubmit={handleSubmit}>
+    <DeckFormStyles className="new-deck-form" onSubmit={handleSubmit}>
       <h2>Crear mazo</h2>
+      <label htmlFor="deckName">Nombre</label>
       <InputStyles 
         required
         type="text"
@@ -20,6 +55,7 @@ export default function NewDeckForm ({ deck = {}, onSubmit }) {
         value={name}
         onChange={ev => setName(ev.target.value)}
         placeholder="Nombre del mazo" />
+      <label htmlFor="deckDescription">Descripci&oacute;n</label>
       <InputStyles 
         as="textarea"
         name="deckDescription"
@@ -30,6 +66,6 @@ export default function NewDeckForm ({ deck = {}, onSubmit }) {
       <Button className="cancel-btn" 
         onClick={() => window.history.back()} 
         type="button">Cancelar</Button>
-    </form>
+    </DeckFormStyles>
   )
 }
--- www/src/pages/DeckEdit.js
+++ www/src/pages/DeckEdit.js
@@ -7,72 +7,11 @@ import useDecks from '../services/useCards'
 import NewDeckForm from '../components/deck-edit/NewDeckForm'
 import CardLists from '../components/deck-edit/CardLists'
 
-import IconArrowLeft from '../components/icons/IconArrowLeft'
-
 const DeckEditStyles = styled.div`
   margin-top: 2rem;
-  position: relative;
-
-  .title {
-    margin: 0;
-    margin-top: 1.5rem;
-  }
-  .description {
-    margin: 0;
-  }
-
-  > header {
-    padding-left: 48px;
-  }
-
-  .back-btn {
-    cursor: pointer;
-    background: none;
-    border: none;
-    padding: 0;
-    position: absolute;
-    top: 12px;
-    left: 0;
-    color: #868686;
-
-    svg {
-      width: 36px;
-      height: 36px;
-    }
-  }
-
-  @media (max-width: 45rem) {
-    .title {
-      font-size: 18px;
-    }
-    .description {
-      font-size: 14px;
-    }
-    .back-btn {
-      top: 8px;
-    }
-  }
-
-  .new-deck-form {  
-    input {
-      max-width: 280px;
-      margin-bottom: 1.5rem;
-    }
-    textarea {
-      margin-bottom: 1.5rem;
-      font: inherit;
-      padding-top: 4px;
-      height: 132px;
-      resize: none;
-    }
-
-    .cancel-btn {
-      background: none;
-      border: none;
-      &:hover, &:focus {
-        text-decoration: underline;
-      }
-    }
+  &.new {
+    display: flex;
+    justify-content: center;
   }
 `
 
@@ -98,29 +37,16 @@ export default function DeckEdit ({ deckid }) {
     setDecks({ ...decks, [id]: { ...deck, cards: deck.cards.filter(c => c.id !== cardid) } })
   }
 
-  if (deckIsNew) {
-    return (
-      <DeckEditStyles>
-        <NewDeckForm onSubmit={editDeck} />
-      </DeckEditStyles>
-    )
-  }
-
   return (
-    <DeckEditStyles>
-      <button className="back-btn" onClick={() => window.history.back()}>
-        <IconArrowLeft />
-      </button>
-      <header>
-        <NewDeckForm deck={deck} onSubmit={editDeck} />
-        {/* <h2 className="title">{deck.name}</h2>
-        <p className="description">{deck.description}</p> */}
-      </header>
-      <CardLists
-        cards={deck.cards.sort((a, b) => (b.created_at || 0) - (a.created_at || 0))}
-        addCard={addCard}
-        removeCard={removeCard}
-        editCard={editCard} />
+    <DeckEditStyles className={deckIsNew ? 'new' : ''}>
+      <NewDeckForm deck={deck} onSubmit={editDeck} />
+      {!deckIsNew && (
+        <CardLists
+          cards={deck.cards.sort((a, b) => (b.created_at || 0) - (a.created_at || 0))}
+          addCard={addCard}
+          removeCard={removeCard}
+          editCard={editCard} />
+      )}
     </DeckEditStyles>
   )
 }
