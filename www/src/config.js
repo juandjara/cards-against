a7b26c774ca3a8ece26323ffff4a2b0c5ef8d252
Juan D. Jara <juanorigami@gmail.com>
2020-07-18 20:46:42 +0200

    choose api for dev or prod

--- www/src/config.js
+++ www/src/config.js
@@ -1,6 +1,7 @@
 export default {
-  api: 'http://localhost:5000',
-  // api: 'https://cards-against-api-socket.herokuapp.com',
+  api: process.env.NODE_ENV === 'production'
+    ? 'https://cards-against-api-socket.herokuapp.com'
+    : 'http://localhost:5000',
   NAME_KEY: 'cards-against-username',
   DATA_KEY: 'cards-against-data'
 }
