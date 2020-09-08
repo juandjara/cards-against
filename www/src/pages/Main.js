113f38fbe69d92fa269d7d06475834c49123676c
Juan D. Jara <juanorigami@gmail.com>
2020-09-09 01:01:43 +0200

    preliminary alert system and global socket events

--- www/src/pages/Main.js
+++ www/src/pages/Main.js
@@ -1,7 +1,27 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import Header from '../components/Header'
+import useGlobalSlice from '../services/useGlobalSlice'
 
 export default function Main ({ children }) {
+  const [socket] = useGlobalSlice('socket')
+  useEffect(() => {
+    socket.on('error', error => {
+      alert('[socket.io] Error from socket', error)
+    })
+    socket.on('disconnect', () => {
+      alert('disconnected from server')
+    })
+    socket.on('reconnect', () => {
+      alert('connected again')
+    })
+
+    return () => {
+      socket.off('error')
+      socket.off('disconnect')
+      socket.off('reconnect')
+    }
+  }, [])
+  
   return (
     <Fragment>
       <Header></Header>
