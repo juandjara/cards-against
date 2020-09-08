a97016ea6968bd8291aae41053aaec67afa359fa
Juan D. Jara <juanorigami@gmail.com>
2020-09-08 02:52:24 +0200

    restore config

--- www/src/config.js
+++ www/src/config.js
@@ -1,8 +1,7 @@
 export default {
-  api: 'http://localhost:5000',
-  // api: process.env.NODE_ENV === 'production'
-  //   ? 'https://cards-against-api-socket.herokuapp.com'
-  //   : 'http://localhost:5000',
+  api: process.env.NODE_ENV === 'production'
+    ? 'https://cards-against-api-socket.herokuapp.com'
+    : 'http://localhost:5000',
   NAME_KEY: 'cards-against-username',
   DATA_KEY: 'cards-against-data',
   TUTORIALS_KEY: 'cards-against-tutorials',
