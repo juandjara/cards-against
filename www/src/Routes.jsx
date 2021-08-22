5623925e77d1fbd5b78764a052dc61d598236ec6
Juan D. Jara <juanorigami@gmail.com>
2021-08-23 00:54:50 +0200

    remove unused

--- www/src/Routes.jsx
+++ www/src/Routes.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes as BrowserRoutes, Route } from 'react-router-dom'
 import Home from '@/views/Home'
-import DeckList from '@/views/DeckList'
 import CreateGame from '@/views/CreateGame'
 import JoinGame from '@/views/JoinGame'
 import PlayGame from '@/views/PlayGame'
@@ -11,7 +10,6 @@ export default function Routes() {
   return (
     <BrowserRoutes>
       <Route path="/" element={<Home />} />
-      <Route path="/decks" element={<DeckList />} />
       <Route path="/newgame" element={<CreateGame />} />
       <Route path="/join/:id" element={<JoinGame />} />
       <Route path="/game/:id" element={<PlayGame />} />
