61b89a2f199427a6835154ba5a65897f1ca82a48
Juan D. Jara <juanorigami@gmail.com>
2021-10-24 01:14:33 +0200

    extract modal component from PlayGame

---
+++ www/src/components/Modal.jsx
@@ -0,0 +1,49 @@
+import React, { Fragment, useEffect, useRef } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import Button from '@/components/Button'
+
+// partly taken from https://headlessui.dev/react/dialog
+export default function Modal({ show, title, children, onClose }) {
+  const closeRef = useRef()
+
+  useEffect(() => {
+    if (closeRef.current) {
+      closeRef.current.focus()
+    }
+  }, [])
+
+  return (
+    <Transition appear show={show} as={Fragment}>
+      <Dialog as="div" initialFocus={closeRef} className="fixed inset-0 z-10 overflow-y-auto" onClose={onClose}>
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100">
+            <Dialog.Overlay className="fixed inset-0" />
+          </Transition.Child>
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span className="inline-block h-screen align-middle" aria-hidden="true">
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+          >
+            <div
+              style={{ minWidth: 300 }}
+              className="inline-block max-w-screen-xl p-4 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl"
+            >
+              <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                {title}
+              </Dialog.Title>
+              {children}
+              <Button ref={closeRef} onClick={onClose} className="mt-4 block ml-auto">
+                Cerrar
+              </Button>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  )
+}
