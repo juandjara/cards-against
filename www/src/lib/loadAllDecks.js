18889b83e989f22b576d4a2602fd9d3adb7f7e3f
Juan D. Jara <juanorigami@gmail.com>
2021-10-27 10:25:13 +0200

    use custom in game creation

--- www/src/lib/loadAllDecks.js
+++ www/src/lib/loadAllDecks.js
@@ -1,6 +1,11 @@
+import { DECKS_KEY } from '@/views/DeckEdit'
+
 const context = import.meta.glob('../assets/*.json')
 
 export default async function loadAllDecks() {
   const promises = Object.keys(context).map(key => context[key]())
-  return Promise.all(promises)
+  const baseDecks = await Promise.all(promises)
+  const savedDecks = JSON.parse(localStorage.getItem(DECKS_KEY) || '[]')
+
+  return baseDecks.concat(savedDecks)
 }
