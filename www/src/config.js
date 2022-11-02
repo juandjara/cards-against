c093a534903f7d72f6c463bd997d16be63099a55
Juan D. Jara <juanorigami@gmail.com>
2022-11-02 11:44:04 +0100

    fuck i did really upload that did i?

--- www/src/config.js
+++ www/src/config.js
@@ -1,4 +1,3 @@
 export default {
-  api: 'http://192.168.1.132:5000'
-  // api: process.env.NODE_ENV === 'production' ? 'https://cards-against.fly.dev' : 'http://localhost:5000'
+  api: process.env.NODE_ENV === 'production' ? 'https://cards-against.fly.dev' : 'http://localhost:5000'
 }
