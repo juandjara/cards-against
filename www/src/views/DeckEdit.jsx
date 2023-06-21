822fa9ef26b917845bbe9fcc2b6a12e3777ecd2d
Juan D. Jara <juanorigami@gmail.com>
2023-06-22 00:36:21 +0200

    refactor card list UI in deck edit page

--- www/src/views/DeckEdit.jsx
+++ www/src/views/DeckEdit.jsx
@@ -6,11 +6,12 @@ import GameCard from '@/components/GameCard'
 import Input from '@/components/Input'
 import PrimaryButton from '@/components/PrimaryButton'
 import Modal from '@/components/Modal'
-import { ArrowLeftIcon, ChevronRightIcon, PencilAltIcon, XIcon } from '@heroicons/react/solid'
+import { ArrowLeftIcon, PencilAltIcon, XIcon } from '@heroicons/react/solid'
 import { Plus, Stack, Trash } from 'phosphor-react'
-import { Disclosure } from '@headlessui/react'
 import classNames from 'classnames'
 import { nanoid } from 'nanoid'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useAlert } from '@/components/Alert'
 
 function decodeHtml(html) {
   var el = document.createElement('textarea')
@@ -26,10 +27,16 @@ function handleStopClick(fn) {
   }
 }
 
-function EditTools({ selection, onNew, onEdit, onDelete, onClear }) {
+function EditTools({ type, selection, onNew, onEdit, onDelete, onClear }) {
   if (selection.length === 0) {
     return (
-      <Button type="button" onClick={handleStopClick(onNew)} className="flex items-center space-x-2 pl-2 pr-3 my-2">
+      <Button
+        type="button"
+        onClick={handleStopClick(onNew)}
+        className="flex items-center space-x-2 pl-2 pr-3 my-2"
+        textColor={type === 'black' ? 'text-white' : 'text-black'}
+        backgroundColor={type === 'black' ? 'bg-black hover:bg-gray-800' : 'bg-white hover:bg-gray-100'}
+      >
         <Plus weight="bold" className="w-4 h-4" />
         <p>Nueva carta</p>
       </Button>
@@ -81,20 +88,11 @@ function CardGroup({ type, cards, setCards }) {
   })
 
   function selectCard(card) {
-    const isSelected = selection.indexOf(card) !== -1
-    const newSelection = isSelected ? selection.filter(c => c !== card) : selection.concat(card)
+    const newSelection = cardIsSelected(card) ? selection.filter(c => c !== card) : selection.concat(card)
     setSelection(newSelection)
   }
 
-  function getCardClass(card) {
-    const animation = 'hover:shadow-lg hover:scale-105 transform-gpu transition'
-    const selected = selection.indexOf(card) !== -1
-    const extra = selected ? 'ring-4 ring-blue-500' : ''
-
-    return `${animation} ${extra}`
-  }
-
-  function update(ev, key) {
+  function updateFormField(ev, key) {
     setNewCard(card => ({ ...card, [key]: ev.target.value }))
   }
 
@@ -131,6 +129,10 @@ function CardGroup({ type, cards, setCards }) {
     setSelection([])
   }
 
+  function cardIsSelected(card) {
+    return selection.indexOf(card) !== -1
+  }
+
   return (
     <>
       <Modal show={showEditForm} onClose={closeModal} title="Editar carta">
@@ -142,7 +144,7 @@ function CardGroup({ type, cards, setCards }) {
               type="number"
               labelColor="text-gray-500"
               value={newCard.pick}
-              onChange={ev => update(ev, 'pick')}
+              onChange={ev => updateFormField(ev, 'pick')}
             />
           )}
           <div>
@@ -153,54 +155,58 @@ function CardGroup({ type, cards, setCards }) {
               id="new-card-text"
               rows="5"
               value={newCard.text}
-              onChange={ev => update(ev, 'text')}
+              onChange={ev => updateFormField(ev, 'text')}
               className="shadow-sm block w-full px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-700 focus:border-blue-700"
             />
           </div>
         </div>
       </Modal>
-      <Disclosure>
-        {({ open }) => (
-          <>
-            <Disclosure.Button
-              as="header"
-              className="cursor-pointer flex flex-wrap items-center w-full sticky top-0 z-10 space-x-3 mb-2 py-2 px-3 bg-gray-500 rounded-lg"
-            >
-              <ChevronRightIcon className={classNames('w-6 h-6', { 'transform rotate-90': open })} />
-              <Stack
-                weight="fill"
-                className={classNames('w-8 h-8', type === 'white' ? 'text-white' : 'text-gray-900')}
-              />
-              <span className="text-lg font-semibold">
-                {label} - <small className="text-sm font-bold">{cards.length}</small>
-              </span>
-              <div className="flex-grow"></div>
-              <EditTools
-                selection={selection}
-                onNew={() => openEditForm(null)}
-                onEdit={() => openEditForm(selection[0])}
-                onDelete={() => deleteCards(selection)}
-                onClear={() => setSelection([])}
-              />
-            </Disclosure.Button>
-            <Disclosure.Panel>
-              <ul className="my-6 grid gap-5 grid-cols-fill-52 place-content-center">
-                {cards.map((card, i) => (
-                  // eslint-disable-next-line
-                  <li key={i} onClick={() => selectCard(card)}>
-                    <GameCard
-                      className={getCardClass(card)}
-                      type={type}
-                      badge={type === 'white' ? null : card.pick}
-                      text={decodeHtml(type === 'white' ? card : card.text)}
-                    />
-                  </li>
-                ))}
-              </ul>
-            </Disclosure.Panel>
-          </>
-        )}
-      </Disclosure>
+      <header className="flex flex-wrap items-center gap-2 pt-6 px-2">
+        <Stack
+          weight="fill"
+          className={classNames('w-7 h-7', {
+            'text-white': type === 'white',
+            'text-black': type === 'black'
+          })}
+        />
+        <span className="text-lg font-semibold flex-grow">
+          {cards.length} {label}
+        </span>
+        <EditTools
+          type={type}
+          selection={selection}
+          onNew={() => openEditForm(null)}
+          onEdit={() => openEditForm(selection[0])}
+          onDelete={() => deleteCards(selection)}
+          onClear={() => setSelection([])}
+        />
+      </header>
+      <div style={{ height: 222 }} className="overflow-hidden">
+        <div className="overflow-x-auto max-w-full">
+          <div className="flex items-start gap-3 p-1 my-1">
+            <AnimatePresence>
+              {cards.map(card => (
+                <GameCard
+                  key={type === 'white' ? card : card.text}
+                  type={type}
+                  text={decodeHtml(type === 'white' ? card : card.text)}
+                  className={classNames('text-left flex-shrink-0 hover:bg-gray-50 focus:outline-none', {
+                    'ring-4 ring-blue-500': cardIsSelected(card)
+                  })}
+                  as={motion.div}
+                  badge={type === 'white' ? null : card.pick}
+                  onClick={() => selectCard(card)}
+                  initial={{ x: 200, opacity: 0, width: 0 }}
+                  animate={{ x: 0, opacity: 1, width: '' }}
+                  exit={{ x: -200, width: 0, opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                  style={{ touchAction: 'pan-x' }}
+                />
+              ))}
+            </AnimatePresence>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
@@ -210,9 +216,15 @@ export const DECKS_KEY = 'CCW_DECKS'
 export default function DeckEdit() {
   const navigate = useNavigate()
   const { id } = useParams()
-  const [title, setTitle] = useState('')
+  const [title, setTitle] = useState(() => {
+    let decks = JSON.parse(localStorage.getItem(DECKS_KEY) || '[]')
+    const deck = decks.find(d => d.id === id)
+    return deck ? deck.name : `Mazo ${decks.length + 1}`
+  })
+
   const [whiteCards, setWhiteCards] = useState([])
   const [blackCards, setBlackCards] = useState([])
+  const alert = useAlert()
 
   useEffect(() => {
     if (id && id !== 'new') {
@@ -244,7 +256,7 @@ export default function DeckEdit() {
     }
 
     localStorage.setItem(DECKS_KEY, JSON.stringify(decks))
-    navigate('/decks')
+    alert({ type: 'success', text: 'Mazo guardado correctamente' })
   }
 
   function deleteDeck() {
@@ -256,47 +268,39 @@ export default function DeckEdit() {
     const newDecks = decks.filter(d => d.id !== id)
 
     localStorage.setItem(DECKS_KEY, JSON.stringify(newDecks))
+    alert({ type: 'success', text: 'Mazo eliminado correctamente' })
     navigate('/decks')
   }
 
   return (
     <Container maxw="container">
-      <div>
-        <Button
-          padding="p-2"
-          className="rounded-full hover:shadow-md mb-4"
-          backgroundColor="bg-white hover:bg-blue-50"
-          onClick={() => navigate(-1)}
-        >
-          <ArrowLeftIcon className="w-5 h-5" />
-        </Button>
-      </div>
-      <h3 className="mb-3 text-3xl font-medium">Editor de mazos</h3>
-      <form className="space-y-10 py-6" onSubmit={handleSubmit}>
-        <div className="max-w-xs">
-          <Input
-            id="name"
-            type="text"
-            label="Título"
-            value={title}
-            onChange={ev => setTitle(ev.target.value)}
-            required
-          />
-        </div>
-        <div className="space-y-8">
-          <p className="text-xl border-b border-white pb-1 pl-1">Cartas</p>
-          <CardGroup type="white" cards={whiteCards} setCards={setWhiteCards} />
-          <CardGroup type="black" cards={blackCards} setCards={setBlackCards} />
-        </div>
-        <div className="border-b border-white my-8" />
-        <div className="flex justify-between">
-          <PrimaryButton type="submit">Guardar</PrimaryButton>
+      <form className="py-6" onSubmit={handleSubmit}>
+        <header className="flex items-center gap-3">
+          <div>
+            <Button
+              padding="p-2"
+              className="rounded-full hover:shadow-md"
+              backgroundColor="bg-white hover:bg-blue-50"
+              onClick={() => navigate(-1)}
+            >
+              <ArrowLeftIcon className="w-5 h-5" />
+            </Button>
+          </div>
+          <h3 className="flex-grow text-3xl font-medium">Editando</h3>
+          <div className="max-w-xs">
+            <Input id="name" type="text" value={title} onChange={ev => setTitle(ev.target.value)} required />
+          </div>
+        </header>
+        <div className="flex justify-end gap-3 mt-3 mb-6">
           {id !== 'new' && (
-            <Button color="red" className="ml-4" onClick={deleteDeck}>
+            <Button color="red" onClick={deleteDeck}>
               Eliminar
             </Button>
           )}
+          <PrimaryButton type="submit">Guardar</PrimaryButton>
         </div>
+        <CardGroup type="white" cards={whiteCards} setCards={setWhiteCards} />
+        <CardGroup type="black" cards={blackCards} setCards={setBlackCards} />
       </form>
     </Container>
   )
