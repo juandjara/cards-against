816631f5135b60e02674b599923791049307a3c1
Juan D. Jara <juanorigami@gmail.com>
2023-07-01 17:05:12 +0200

    include disabled card logic in game creator

--- www/src/lib/loadAllDecks.js
+++ www/src/lib/loadAllDecks.js
@@ -2,10 +2,21 @@ import { DECKS_KEY } from '@/views/DeckEdit'
 
 const context = import.meta.glob('../assets/*.json')
 
+function clearDeck(deck) {
+  const { whiteCards, blackCards, disabledWhites, disabledBlacks } = deck
+  const filteredWhite = whiteCards.filter((card, idx) => !disabledWhites.includes(idx))
+  const filteredBlack = blackCards.filter((card, idx) => !disabledBlacks.includes(idx))
+  return {
+    ...deck,
+    whiteCards: filteredWhite,
+    blackCards: filteredBlack
+  }
+}
+
 export default async function loadAllDecks() {
   const promises = Object.keys(context).map(key => context[key]())
   const baseDecks = await Promise.all(promises)
   const savedDecks = JSON.parse(localStorage.getItem(DECKS_KEY) || '[]')
 
-  return baseDecks.concat(savedDecks)
+  return baseDecks.concat(savedDecks).map(clearDeck)
 }
